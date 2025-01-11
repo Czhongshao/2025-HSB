bebddb0a271e84b542d4442fc4a31ccaf5caa8f4
zhongshao <145018310+Czhongshao@users.noreply.github.com>
2025-01-11 17:36:34 +0800

    更新图像

--- code/q1/q1.py
+++ code/q1/q1.py
@@ -17,7 +17,7 @@ df=pd.read_excel('../../data/近十年各行业生产总值数据表.xlsx',sheet
 
 # 选择特定的列来创建一个新的DataFrame
 df1 = df[['国内生产总值', '农林牧渔业', '工业', '建筑业', '批发和零售业', '交通运输、仓储和邮政业', '住宿和餐饮业', '金融业', '房地产业', '其他']]
-df2 = df[['国内生产总值', '农林牧渔业', '工业', '建筑业', '批发和零售业', '交通运输、仓储和邮政业', '住宿和餐饮业', '金融业', '房地产业', '其他']]
+# df2 = df[['国内生产总值', '农林牧渔业', '工业', '建筑业', '批发和零售业', '交通运输、仓储和邮政业', '住宿和餐饮业', '金融业', '房地产业', '其他']]
 
 # 中英文列标题对照
 industry_names1 = {
@@ -34,18 +34,18 @@ industry_names1 = {
 }
 
 # 分析中英对照表
-industry_names2 = {
-    '国内生产总值': 'GDP',
-    '农林牧渔业': 'Farming_Forestry_Livestock_and_Fishing',
-    '工业': 'Industry',
-    '建筑业': 'Construction',
-    '批发和零售业': 'Wholesale_and_Retail_Trade',
-    '交通运输、仓储和邮政业': 'Transportation_Warehousing_and_Postal_Services',
-    '住宿和餐饮业': 'Accommodation_and_Catering',
-    '金融业': 'Finance',
-    '房地产业': 'Real_Estate',
-    '其他': 'Others'
-}
+# industry_names2 = {
+#     '国内生产总值': 'GDP',
+#     '农林牧渔业': 'Farming_Forestry_Livestock_and_Fishing',
+#     '工业': 'Industry',
+#     '建筑业': 'Construction',
+#     '批发和零售业': 'Wholesale_and_Retail_Trade',
+#     '交通运输、仓储和邮政业': 'Transportation_Warehousing_and_Postal_Services',
+#     '住宿和餐饮业': 'Accommodation_and_Catering',
+#     '金融业': 'Finance',
+#     '房地产业': 'Real_Estate',
+#     '其他': 'Others'
+# }
 
 # 使用映射将中文列标题转换为英文
 df1.rename(columns=industry_names1, inplace=True)
@@ -55,8 +55,10 @@ df1.rename(columns=industry_names1, inplace=True)
 correlation_matrix = df1.corr()
 plt.figure(figsize=(10,10))
 sns.heatmap(correlation_matrix, vmax=1, square=True,annot=True,cmap='YlOrRd')
-plt.title("Industry Correlation Matrix(行业相关性矩阵1)")
-plt.show()
+# plt.title("Industry Correlation Matrix")
+# 保存图像
+plt.savefig("../../img/Industry_Correlation_Matrix.png", dpi=500)
+# plt.show()
 
 
 # 相关性分析2：根据经济的增长率数值进行相关性分析
@@ -69,8 +71,10 @@ correlation_growth = df_growth.corr()
 # 绘制增长率的相关性热力图
 plt.figure(figsize=(12, 10))
 sns.heatmap(correlation_growth, vmax=1, vmin=-1, square=True, annot=True, cmap='coolwarm', linewidths=0.5)
-plt.title("Growth Rate Correlation Matrix(行业相关性矩阵2)")
-plt.show()
+# plt.title("Growth Rate Correlation Matrix")
+# 保存图像
+plt.savefig("../../img/Growth_Rate_Correlation_Matrix.png", dpi=500)
+# plt.show()
 
 
 # from statsmodels.formula.api import ols
